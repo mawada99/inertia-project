569a8acd138ecce65b7a774309501f9ce548acea
mawada ahmed <mawadaahmed973@gmail.com>
2024-12-11 09:04:00 +0200

    homePage

---
+++ resources/js/Pages/Home/HomePage.jsx
@@ -0,0 +1,13 @@
+import Layout from './Layout'
+import { Head } from "@inertiajs/react";
+
+export default function HomePage({}) {
+    return (
+   <div>
+            <Head title="Welcome HomePage" />
+            <h1>Welcome</h1>
+            <p>Hello, welcome to your first Inertia app!</p>
+          </div>
+       
+    );
+}
